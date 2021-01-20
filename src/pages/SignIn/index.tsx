d53f95bf790ad053a46a02239bee0bdeca6ba827
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2021-01-20 19:23:39 -0300

    :sparkles: add forgotpassword and create account elements

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,32 +1,55 @@
 import React from 'react';
 import { Image } from 'react-native';
-
+import Icon from 'react-native-vector-icons/Ionicons';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
+    <>
+      <Container>
+        <Image source={logoImg} />
+        <Title>Faça seu logon</Title>
 
-      <Input name="email" icon="mail" placeholder="Email" />
+        <Input name="email" icon="mail" placeholder="Email" />
 
-      <Input name="password" icon="lock-closed" placeholder="Password" />
+        <Input name="password" icon="lock-closed" placeholder="Password" />
 
-      <Button
-        icon="log-in"
+        <Button
+          onPress={() => {
+            console.log('clickable');
+          }}
+        >
+          Entrar
+        </Button>
+        <ForgotPassword
+          onPress={() => {
+            console.log('forgotpassword');
+          }}
+        >
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+      <CreateAccountButton
         onPress={() => {
-          console.log('clickable');
+          console.log('createaccountbutton');
         }}
       >
-        Entrar
-      </Button>
-    </Container>
+        <Icon name="log-in" size={20} color="rgb(0, 207, 255)" />
+        <CreateAccountText>Criar minha conta</CreateAccountText>
+      </CreateAccountButton>
+    </>
   );
 };
 
