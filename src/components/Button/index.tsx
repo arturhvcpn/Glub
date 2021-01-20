2bc84d67bc6feaeb9fbec1def52a5a3a593b92e3
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2021-01-20 14:44:34 -0300

    :sparkles: create component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, ButtonText } from './styles';
 
-const Button: React.FC = () => {
-  return <Container />;
+const Button: React.FC = ({ children }) => {
+  return (
+    <Container>
+      <ButtonText>{children}</ButtonText>
+    </Container>
+  );
 };
 
 export default Button;
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background-color: rgb(0, 207, 255);
+  border-radius: 30px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'Roboto-Bold';
+  color: #fff;
+  font-size: 18px;
+`;
