8de34540515b298b6e90a3e51af1ee4c14151561
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2021-01-23 11:06:12 -0300

    :sparkles: create page SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInText,
+} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+            <Input name="user" icon="person" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="Email" />
+
+            <Input name="password" icon="lock-closed" placeholder="Password" />
+
+            <Button
+              onPress={() => {
+                console.log('clickable');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignInButton
+        onPress={() => {
+          console.log('backtosignIn');
+        }}
+      >
+        <Icon name="arrow-back-outline" size={20} color="#fff" />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignInButton>
+    </>
+  );
 };
 
 export default SignUp;
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #191919;
+  padding: 0 30px ${Platform.OS === 'android' ? 140 : 20}px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #ffffff;
+  font-family: 'Roboto-Medium';
+  margin: 64px 0 24px;
+`;
+
+export const BackToSignInButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgb(31, 31, 31);
+  border-top-width: 1px;
+  border-color: rgb(28, 28, 28);
+  padding: 16px 0 ${10 + getBottomSpace()}px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const BackToSignInText = styled.Text`
+  color: #fff;
+  font-family: 'Roboto-Regular';
+  font-size: 18px;
+  margin-left: 8px;
+`;
